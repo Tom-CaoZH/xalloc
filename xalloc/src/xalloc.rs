8103b41c545fafaddd6041cdc3f3653702908a7e
Tom-CaoZH <tomcaottt@outlook.com>
2023-09-23 05:25:53 -0700

    add EX_MEM allocation init

--- xalloc/src/xalloc.rs
+++ xalloc/src/xalloc.rs
@@ -1,12 +1,18 @@
 // src/xalloc.rs
 use tikv_jemalloc_sys;
 
+extern crate nix;
+use linux_support::libnuma;
+use std::process;
+use nix::sys::pthread::pthread_self;
+use std::os::raw::c_int;
+
 #[cfg(not(target_env = "msvc"))]
 use tikv_jemalloc_sys::{mallocx, malloc, free, MALLOCX_ARENA, MALLOCX_TCACHE, MALLOCX_TCACHE_NONE};
 
 use std::mem;
 use std::os::raw::c_void;
-
+use std::sync::Once;
 
 pub enum MemoryType {
     NORMAL,
@@ -15,9 +21,57 @@ pub enum MemoryType {
 
 pub struct XAllocator {
     memory_type: MemoryType,
+    init_once: Once, 
 }
 
+
 impl XAllocator {
+    fn get_fs_base() -> usize {
+        pthread_self() as usize
+    }
+
+    fn xalloc_thread_get_arena(arena: &mut u32, size: usize) -> i32 {
+        let arena_idx = hash64(get_fs_base()) & kind.arena_map_mask;
+        *arena = kind.arena_zero + arena_idx;
+        0
+    }
+
+    fn xalloc_ex_mem_init() {
+        println!("Initializing ex_mem");
+
+        if let Err(e) = numa::available() {
+            eprintln!("Error: {:?}", e);
+            process::abort(); // Abort the program in case of any error
+        }
+
+        println!("NUMA available");
+    }
+
+    fn init_xallocator(&self) {
+        self.init_once.call_once(|| {
+            xalloc_ex_mem_init();
+        });
+    }
+
+    fn allocate_ex_mem(size: usize) -> *mut u8 {
+        // Implement EX_MEM allocation logic here
+        
+        
+        let mut arena: c_int = 0;
+        let flags = MALLOCX_ARENA(arena as usize) | get_tcache_flag((*kind).partition, size);
+        let mut ptr: *mut c_void = std::ptr::null_mut();
+        unsafe {
+            ptr = mallocx(size, flag);
+        }
+        if ptr.is_null() {
+            panic!("EXMEM memory allocation failed");
+        }
+        let ptr = unsafe {
+            mem::transmute::<*mut c_void, *mut u8>(malloc(size))
+        };
+        ptr
+    }
+
     pub fn new(memory_type: MemoryType) -> Self {
         Self { memory_type }
     }
@@ -31,7 +85,7 @@ impl XAllocator {
                     ptr = malloc(size);
                 }
                 if ptr.is_null() {
-                    panic!("Memory allocation failed");
+                    panic!("NORMAL Memory allocation failed");
                 }
                 let ptr = unsafe {
                     mem::transmute::<*mut c_void, *mut u8>(malloc(size))
@@ -39,8 +93,7 @@ impl XAllocator {
                 ptr
             }
             MemoryType::EXMEM => {
-                // Implement EX_MEM allocation logic here
-                unimplemented!("EX_MEM allocation not implemented");
+                allocate_ex_mem(size)
             }
         }
     }
