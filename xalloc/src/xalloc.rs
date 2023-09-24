bc104d4567a342e479341d458849bfc3d26e7cea
Tom-CaoZH <tomcaottt@outlook.com>
2023-09-23 21:54:01 -0700

    fix

--- xalloc/src/xalloc.rs
+++ xalloc/src/xalloc.rs
@@ -2,17 +2,17 @@
 use tikv_jemalloc_sys;
 
 extern crate nix;
-use linux_support::libnuma;
+// use libnuma::numa;
 use std::process;
 use nix::sys::pthread::pthread_self;
 use std::os::raw::c_int;
 
 #[cfg(not(target_env = "msvc"))]
-use tikv_jemalloc_sys::{mallocx, malloc, free, MALLOCX_ARENA, MALLOCX_TCACHE, MALLOCX_TCACHE_NONE};
+use tikv_jemalloc_sys::{mallocx, malloc, free, MALLOCX_ARENA};
 
 use std::mem;
 use std::os::raw::c_void;
-use std::sync::Once;
+// use std::sync::Once;
 
 pub enum MemoryType {
     NORMAL,
@@ -21,7 +21,9 @@ pub enum MemoryType {
 
 pub struct XAllocator {
     memory_type: MemoryType,
-    init_once: Once, 
+    // init_once: Once, 
+    arena_zero: usize,
+    arena_map_mask: usize, 
 }
 
 
@@ -30,35 +32,17 @@ impl XAllocator {
         pthread_self() as usize
     }
 
-    fn xalloc_thread_get_arena(arena: &mut u32, size: usize) -> i32 {
-        let arena_idx = hash64(get_fs_base()) & kind.arena_map_mask;
-        *arena = kind.arena_zero + arena_idx;
-        0
-    }
-
-    fn xalloc_ex_mem_init() {
-        println!("Initializing ex_mem");
-
-        if let Err(e) = numa::available() {
-            eprintln!("Error: {:?}", e);
-            process::abort(); // Abort the program in case of any error
-        }
-
-        println!("NUMA available");
-    }
-
-    fn init_xallocator(&self) {
-        self.init_once.call_once(|| {
-            xalloc_ex_mem_init();
-        });
+    fn xalloc_thread_get_arena(arena: &mut u32) {
+        let arena_idx = hash64(get_fs_base()) & self.arena_map_mask;
+        *arena = self.arena_zero + arena_idx;
     }
 
     fn allocate_ex_mem(size: usize) -> *mut u8 {
         // Implement EX_MEM allocation logic here
         
-        
         let mut arena: c_int = 0;
-        let flags = MALLOCX_ARENA(arena as usize) | get_tcache_flag((*kind).partition, size);
+        xalloc_thread_get_arena(&arena);
+        let flags = MALLOCX_ARENA(arena as usize);
         let mut ptr: *mut c_void = std::ptr::null_mut();
         unsafe {
             ptr = mallocx(size, flag);
